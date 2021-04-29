d70dcb6f8768cfc3093ca2b210a03353b0e1a545
Husnul Fadhilah <36095304+husnul23@users.noreply.github.com>
2021-04-29 09:04:13 +0700

    added upload logic to handle upload request

--- wesharing/sharing/views.py
+++ wesharing/sharing/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
+from .models import File
+from .forms import UploadForm
 
 def index(request):
     return render(request, 'sharing/index.html')
 
 def upload(request):
+    if request.method == 'POST':
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            return HttpResponseRedirect('/download/')
+
     return render(request, 'sharing/index.html')
 
 def download(request):
