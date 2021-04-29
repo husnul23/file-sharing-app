90873fe4d78b677550e3a9c490479638c0a74aa1
Husnul Fadhilah <36095304+husnul23@users.noreply.github.com>
2021-04-29 10:42:52 +0700

    added prefix url

--- wesharing/wesharing/urls.py
+++ wesharing/wesharing/urls.py
@@ -1,20 +1,9 @@
-"""wesharing URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
+
 from sharing import views
 
 urlpatterns = [
@@ -22,4 +11,4 @@ urlpatterns = [
     path('<uuid:uid>/', views.download),
     path('', views.index),
     # path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
