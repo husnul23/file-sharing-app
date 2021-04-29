37e69d61041f49be4b2747f731c68c84f5a68de3
Husnul Fadhilah <36095304+husnul23@users.noreply.github.com>
2021-04-29 11:42:49 +0700

    add aws s3 config

--- wesharing/wesharing/settings.py
+++ wesharing/wesharing/settings.py
@@ -118,6 +118,12 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+# Uncomment lines di bawah ini kalau ingin menggunakan AWS S3
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+AWS_STORAGE_BUCKET_NAME = ''
+AWS_IS_GZIPPED =True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
