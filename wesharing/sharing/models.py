82c983951b281833d33238172563b80aee27b3d1
Husnul Fadhilah <36095304+husnul23@users.noreply.github.com>
2021-04-29 09:03:52 +0700

    fix model

--- wesharing/sharing/models.py
+++ wesharing/sharing/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 def handle_upload_to(instance, filename):
-    return '()/()'.format(instance.uid, filename)
+    return '{}/{}'.format(instance.uid, filename)
 
 
 class File(models.Model):
     uid = models.CharField(max_length=200)
-    model = models.FileField(upload_to=handle_upload_to)
+    file = models.FileField(upload_to=handle_upload_to)
